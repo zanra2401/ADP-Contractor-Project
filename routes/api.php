99018140284f90a0c5b4fdbbb15e30e17a5f6552
Pras7804 <dwiprasetyamumtaz@gmail.com>
2025-11-30 17:17:36 +0700

    backend route

--- routes/api.php
+++ routes/api.php
@@ -9,6 +9,10 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\UserManagementController;
 use App\Http\Controllers\Admin\DesignController;
 use App\Http\Controllers\Admin\ProgressApprovalController;
+use App\Http\Controllers\SuperAdminController;
+use App\Http\Controllers\ProgressController;
+use App\Http\Controllers\Auth\ForgotPasswordController;
+use App\Http\Controllers\Auth\AdminProjectController;
 
 // ==========================================
 //   Group khusus ADMIN (prefix: /api/admin)
@@ -39,4 +43,47 @@ Route::prefix('admin')->group(function () {
     Route::get('/progress/pending',           [ProgressApprovalController::class, 'pending']);
     Route::post('/progress/{id}/approve',     [ProgressApprovalController::class, 'approve']);
     Route::post('/progress/{id}/reject',      [ProgressApprovalController::class, 'reject']);
+
+});
+
+
+Route::prefix('superadmin')->group(function () {
+    // ====================================
+    //    CRUD USER (SUPERADMIN)
+    // ====================================
+    Route::get('/users', [SuperAdminController::class, 'index']);
+    Route::post('/users', [SuperAdminController::class, 'store']);
+    Route::get('/users/{id}', [SuperAdminController::class, 'show']);
+    Route::put('/users/{id}', [SuperAdminController::class, 'update']);
+    Route::delete('/users/{id}', [SuperAdminController::class, 'destroy']);
+});
+
+
+Route::middleware(['auth:sanctum'])->group(function () {
+    // ====================================
+    //    CRUD Progress Proyek API (ADMIN)
+    // ====================================
+    Route::post('/progress', [ProgressController::class, 'store']);
+    Route::get('/progress/project/{projectId}', [ProgressController::class, 'listByProject']);
+    Route::get('/progress/{id}', [ProgressController::class, 'show']);
+    Route::put('/progress/{id}', [ProgressController::class, 'update']);
+    Route::delete('/progress/{id}', [ProgressController::class, 'destroy']);
+});
+
+
+// ====================================
+//    FORGOT PASSWORD API
+// ====================================
+Route::post('/forgot-password', [ForgotPasswordController::class, 'requestReset']);
+Route::post('/reset-password', [ForgotPasswordController::class, 'resetPassword']);
+
+
+Route::middleware('auth:sanctum')->prefix('admin')->group(function () {
+    // ====================================
+    //    CRUD Project Proyek API (ADMIN)
+    // ====================================
+    Route::get('/projects', [AdminProjectController::class, 'index']);
+    Route::post('/projects', [AdminProjectController::class, 'store']);
+    Route::put('/projects/{project}', [AdminProjectController::class, 'update']);
+    Route::delete('/projects/{project}', [AdminProjectController::class, 'destroy']);
 });
