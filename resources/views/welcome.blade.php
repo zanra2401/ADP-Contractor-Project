49c46911cc36882048d986794d7ef75abf03663d
zanra2401 <grubgrex@gmail.com>
2025-12-14 11:37:19 +0700

    update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -96,7 +96,7 @@
     <footer class="bg-gray-800 text-white py-8 mt-10">
         <div class="max-w-7xl mx-auto px-4 text-center">
             <p>&copy; 2025 ADP Konstruksi. All rights reserved.</p>
-        </div>
+    </div>
     </footer>
 
 </body>
